df8deefd89d9283f0a4f9fc760bcf85a0cdb3945
Trillaman <26063289+trillaman@users.noreply.github.com>
2022-01-10 11:22:05 +0100

    Separated wp_downloader to class

--- ss_auto.py
+++ ss_auto.py
@@ -1,88 +1,25 @@
 import os
 import re
-import time
 from dotenv import load_dotenv
-import requests
-import time
-import pathlib
-from bs4 import BeautifulSoup
-
-# PRE CHECKS
-folder = pathlib.Path("./wp-plugins-downloaded")
-if folder.exists() is False:
-        os.system("mkdir wp-plugins-downloaded")
-
-folder = pathlib.Path("wp-plugins-extracted")
-if folder.exists() is False:
-        os.system("mkdir wp-plugins-extracted")
 
 load_dotenv()
 
-# SCRAPING WORDPRESS PLUGINS PAGE
-URL = "https://pl.wordpress.org/plugins/browse/blocks/"  # main plugins site
-page = requests.get(URL)  # we are downloading all html from first page
-
-plugins = []
-plugins_urls = []
-plugins_zips = []
 
 class_list = set()
 
-soup = BeautifulSoup( page.content , 'html.parser')  # here we are parsing this
-
-tags = {tag.name for tag in soup.find_all()}  # and selecting all tags
 
 
-h3 = soup.findAll('h3', {'class': 'entry-title'})  # in those tags we are looking for h3 entry title which means name of the plugin
-
-i = 0
-for child_a in h3:  # this loop is for getting plugin names and urls to plugin sites
-        children = child_a.findChildren("a" , recursive=False)  # here we are looking for link to plugin site which is taken from clickable header
-        plugins_urls.append(children[0]['href'])  # and we are adding this to plugin_urls - in this array we will have all link to separated plugins sites
-        plugin_name = str(''.join(filter(str.isalnum, child_a.string)))  # here we are removing special characters from title if any
-        plugin_name = plugin_name[:10].replace(" ", "")  # and here we are trimming to 10 signs from beginning and removing spaces
-        plugins.append(plugin_name)  # here we got clear plugin names for sonnar project
-        print(plugins[i])
-        print(plugins_urls[i])
-        i +=1 
-        time.sleep(1)
 
 
-# NOW GO THROUGH PLUGINS SITES TO GET ZIP LINK
 
-for i in range(len(plugins)):  # for every plugin
-        plugin_url = plugins_urls[i]  # we get this url
-        plugin_page = requests.get(plugin_url)  # we are downloading page for specific plugin
-        soup = BeautifulSoup( plugin_page.content , 'html.parser')  # and parsing this
-        #print(soup)
-        plugins_zip_url = soup.findAll('a', {'class': 'plugin-download'})  # here we are exrtracting download link to plugin which is leading to zip file
-        zip_href = plugins_zip_url[0]['href'] # and here we got clear href
-        plugins_zips.append(plugins_zip_url[0]['href'])
-        print(str(plugins_zip_url[0]['href']) + " added to array")
-        time.sleep(1)
 
-# Downloading zips
 
-index = 0
 
-for el in plugins_zips:
-        print("Downloading " + plugins[index])
-        curl_cmd = "curl --output ./wp-plugins-downloaded/" + plugins[index] + " --url " + el
-        curl_cmd = str(curl_cmd)
-        os.system(curl_cmd)
-        print("Downloaded " + plugins[index])
-
-        print("Unzipping " + plugins[index])
-        unzip_cmd = "unzip -d ./wp-plugins-extracted/ ./wp-plugins-downloaded/" + plugins[index]
-        os.system(unzip_cmd)
-        print("Plugin " + plugins[index] + " unzipped")
-
-        index += 1
 
 
 # Creating sonar-scanner.properties for each folder
 
-os.system("find ./wp-plugins-extracted -maxdepth 1 -type d | grep 'wp-plugins-extracted/' > ./wp-plugins-extracted/list_of_folders.txt")
+
 
 file_with_list = open("./wp-plugins-extracted/list_of_folders.txt", "r")
 
---
+++ wp_downloader.py
@@ -0,0 +1,107 @@
+import pathlib
+import os
+import requests
+from bs4 import BeautifulSoup
+import time
+import sys
+
+
+class WP_Plugin_Downloader:
+    URL = "https://pl.wordpress.org/plugins/browse/blocks/"  # main plugins site
+
+    page = requests.get(URL)  # we are downloading all html from first page
+
+    plugins = []  # for storing plugins names
+    plugins_urls = []  # for storing plugin urls
+    plugins_zips = []  # for storing plugins zip urls
+
+    soup = BeautifulSoup(page.content, 'html.parser')  # here we are parsing this
+    tags = {tag.name for tag in soup.find_all()}  # and selecting all tags
+    h3 = soup.findAll('h3', {
+        'class': 'entry-title'})  # in those tags we are looking for h3 entry title which means name of the plugin
+
+    def check_wp_folders(self):
+
+        if pathlib.Path("./wp-plugins-downloaded").exists() is False:
+            try:
+                os.system("mkdir wp-plugins-downloaded")
+                print("Folder wp-plugins-downloaded created")
+            except Exception:
+                print("Can't create folder wp-plugins-downloaded")
+                sys.exit()
+
+        if pathlib.Path("./wp-plugins-extracted").exists() is False:
+            try:
+                os.system("mkdir wp-plugins-extracted")
+                print("Folder wp-plugins-extracted created")
+            except Exception:
+                print("Can't create folder wp-plugins-extracted")
+                sys.exit()
+
+        return True
+
+    def get_plugins_urls(self):
+        i = 0
+        for child_a in self.h3:  # this loop is for getting plugin names and urls to plugin sites
+            children = child_a.findChildren("a",
+                                            recursive=False)  # here we are looking for link to plugin site which is taken from clickable header
+            self.plugins_urls.append(children[0][
+                                         'href'])  # and we are adding this to plugin_urls - in this array we will have all link to separated plugins sites
+            plugin_name = str(''.join(
+                filter(str.isalnum, child_a.string)))  # here we are removing special characters from title if any
+            plugin_name = plugin_name[:10].replace(" ",
+                                                   "")  # and here we are trimming to 10 signs from beginning and removing spaces
+            self.plugins.append(plugin_name)  # here we got clear plugin names for sonnar project
+            print(self.plugins[i])
+            print(self.plugins_urls[i])
+            i += 1
+            time.sleep(1)
+
+        return True
+
+    # NOW GO THROUGH PLUGINS SITES TO GET ZIP LINK
+    def get_plugin_zip_urls(self):
+
+        for i in range(len(self.plugins)):  # for every plugin
+            plugin_url = self.plugins_urls[i]  # we get this url
+            plugin_page = requests.get(plugin_url)  # we are downloading page for specific plugin
+            soup = BeautifulSoup(plugin_page.content, 'html.parser')  # and parsing this
+            # print(soup)
+            plugins_zip_url = soup.findAll('a', {
+                'class': 'plugin-download'})  # here we are exrtracting download link to plugin which is leading to zip file
+            zip_href = plugins_zip_url[0]['href']  # and here we got clear href
+            self.plugins_zips.append(zip_href)
+            print(str(zip_href) + " added to array")
+            time.sleep(1)
+
+        return True
+
+    # Downloading zips
+    def download_wp_plugins(self):
+        index = 0
+
+        for el in self.plugins_zips:
+            print("Downloading " + self.plugins[index])
+            curl_cmd = "curl --output ./wp-plugins-downloaded/" + self.plugins[index] + " --url " + el
+            curl_cmd = str(curl_cmd)
+            os.system(curl_cmd)
+            print("Downloaded " + self.plugins[index])
+
+            print("Unzipping " + self.plugins[index])
+            unzip_cmd = "unzip -d ./wp-plugins-extracted/ ./wp-plugins-downloaded/" + self.plugins[index]
+            os.system(unzip_cmd)
+            print("Plugin " + self.plugins[index] + " unzipped")
+
+            index += 1
+
+        return True
+
+    def create_list_of_folders(self):
+        try:
+            os.system(
+                "find ./wp-plugins-extracted -maxdepth 1 -type d | grep 'wp-plugins-extracted/' > ./wp-plugins-extracted/list_of_folders.txt")
+            sys.exit()
+        except Exception:
+            print("Failed to create list_of_folders.txt")
+
+        return True
